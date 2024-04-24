f184d558910896bb683b36c03d66e18e95d08260
Gustavo Martínez <gusmbox@gmail.com>
2024-04-25 01:01:29 +0200

    front-page added

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<section class="container-fluid" id="landing">
+        <div class="container" id="landing-content">
+            <div class="row h-100">
+                <div class="col-lg-6 col-xs-0"></div>
+                <div class="col-lg-6 col-xs-12 d-flex align-items-center">
+                    <div class="">
+                        <p id="artist-name">Ezequiel Delanada</p>
+                        <p id="artist-mini-desc">Productor & DJ</p>
+                    </div>
+                </div>     
+            </div>
+        </div>
+</section>
+
+<?php get_footer(); ?>
