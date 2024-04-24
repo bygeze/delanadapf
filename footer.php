7692068827a9b6e510fc10f1bb8503133d4470cf
Gustavo Martínez <gusmbox@gmail.com>
2024-04-24 22:12:29 +0200

    transforming the static website to a wp template

---
+++ footer.php
@@ -0,0 +1,10 @@
+    <section id="footer">
+        <div class="container h-100 d-flex align-items-center justify-content-center text-center">
+            <p class="m-0 text-center text-white">Copyright &copy; <?php bloginfo('name'); echo ' '.date('Y'); ?> </p>
+            <?php wp_footer(); ?>        
+        </div>
+    </section>  
+
+    <script src="lib/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <?php wp_head(); ?>
+
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <section class="container-fluid" id="topbar">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <a href="#"><img id="logo" src="img/logo-256.png"></a>    
+                </div>
+                <div class="col-6 d-flex align-items-center justify-content-end">
+                    <div class="social-media ">
+                        <a href="https://soundcloud.com/ezequiel-delanada"><img src="img/soundcloud.png" alt=""></a>
+                        <a href="https://open.spotify.com/artist/7AH746wyI5u5gjRK6Thauj?si=Vk_kzWHpSe6X9rtHtw5DlQ"><img src="img/spotify.png" alt=""></a>
+                        <a href="https://www.instagram.com/delanada.wav"><img src="img/instagram.png" alt=""></a>        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
