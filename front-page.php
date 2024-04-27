9a56a0148a74200e9910286854cb9ca526c5611f
Gustavo Martinez <gusmbox@gmail.com>
2024-04-27 17:10:18 +0200

    background overlay

--- front-page.php
+++ front-page.php
@@ -1,6 +1,13 @@
 <?php get_header(); ?>
 
-<section class="container-fluid" id="landing"  style="background-image: url(<?php echo get_template_directory_uri(); ?>/img/bg-landing.jpg);">
+<section 
+    class="container-fluid" 
+    id="landing"  
+    style="background-image: 
+    linear-gradient(180deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), 
+    linear-gradient(180deg, hsla(0, 0%, 100%, 0.2), hsla(0, 0%, 100%, 0.2)), 
+    linear-gradient(180deg, rgba(47, 0, 90, 0.3), rgba(47, 0, 90, 0.3)),
+    url(<?php echo get_template_directory_uri(); ?>/img/bg-landing.jpg);">
         <div class="container" id="landing-content">
             <div class="row h-100">
                 <div class="col-lg-6 col-xs-0"></div>
