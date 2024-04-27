7e4599ad73ff8d4e7eec0521e5321bec5a217b02
Gustavo Martínez <gusmbox@gmail.com>
2024-04-27 11:18:38 +0200

    test 3 automated repo

--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@
                 <div class="col-lg-6 col-xs-0"></div>
                 <div class="col-lg-6 col-xs-12 d-flex align-items-center">
                     <div class="">
-                        <p id="artist-name">Ezequiel Delanada</p>
+                        <p id="artist-name"></p>
                         <p id="artist-mini-desc">Productor & DJ</p>
                     </div>
                 </div>     
