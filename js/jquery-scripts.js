4995b0418f81fcc823127010a718a773b545e58c
Gustavo Martinez <gusmbox@gmail.com>
2024-04-27 17:39:36 +0200

    rotating logo

--- js/jquery-scripts.js
+++ js/jquery-scripts.js
@@ -1,14 +1,8 @@
 $(document).ready(function() {
     // Function to rotate the logo on Y axis
     function rotateLogo() {
-        $("#logo").animate({
-            // Rotate 360 degrees around the Y axis
-            transform: 'rotateY(360deg)'
-        }, 1000, function() {
-            // Animation complete
-            // Reset the rotation to 0 degrees
-            $(this).css('transform', 'rotateY(0deg)');
-        });
+        // Toggle a CSS class to trigger the rotation animation
+        $("#logo").toggleClass('rotate');
     }
 
     // Call the rotateLogo function on click or any other event
