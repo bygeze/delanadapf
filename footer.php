9e78445b296dfb7091eadda0b18f6e8d980d01d9
Gustavo Martinez <gusmbox@gmail.com>
2024-04-27 17:33:16 +0200

    footer quick fix .js

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
   integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
   crossorigin="anonymous"></script>
 
-  <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-scripts.png"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-scripts.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
